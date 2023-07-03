a54bbf5232ff1e08e63618cf59292e7f74d75856
Mateo Catalano <100876278+MateoCata98@users.noreply.github.com>
2023-07-03 15:56:18 -0300

    Update userController.ts

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,1 +1,80 @@
+import { Request, Response } from "express";
+import {
+  authenticateUser,
+  createUser,
+  updateUser,
+  deleteOneUser,
+  getAllUsers,
+  getAllActiveUsers
+} from "../services/userServices";
+
+export async function register(req: Request, res: Response): Promise<Response> {
+  try {
+    const newUser = await createUser(req.body);
+    return res.status(201).send({ newUser });
+  } catch (error) {
+    return res.status(400).send({ message: (error as Error).message });
+  }
+}
+
+export async function login(req: Request, res: Response): Promise<Response> {
+  try {
+    const { email, password } = req.body;
+    const { token, user } = await authenticateUser(email, password);
+    return res.status(201).send({ token, user, message: "Session started" });
+  } catch (error) {
+    return res.status(401).send({ message: (error as Error).message });
+  }
+}
+
+export async function editUser(req: Request, res: Response): Promise<Response> {
+  try {
+    const userId = req.params.id;
+    const editedUser = await updateUser(userId, req.body);
+    return res
+      .status(200)
+      .send({ editedUser, message: "User edited successfully" });
+  } catch (error) {
+    return res.status(401).send({ message: (error as Error).message });
+  }
+}
+
+export async function deleteUser(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  try {
+    const userId = req.params.id;
+    await deleteOneUser(userId);
+    return res.status(200).send({ message: "User deleted" });
+  } catch (error) {
+    return res.status(401).send({ message: (error as Error).message });
+  }
+}
+
+export async function getAllActiveDeliveries(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  try {
+    const allActiveUsers = await getAllActiveUsers();
+    return res
+      .status(200)
+      .send({ allActiveUsers, message: "All active users" });
+  } catch (error) {
+    return res.status(404).send({ message: (error as Error).message });
+  }
+}
+
+export async function getAllDeliveries(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  try {
+    const allUsers = await getAllUsers();
+    return res.status(200).send({ allUsers, message: "All Users" });
+  } catch (error) {
+    return res.status(404).send({ message: (error as Error).message });
+  }
+}
 
