342db71ce19c70889aa5c14eed4f8d33385179ee
BelenCecere <cecere3329@gmail.com>
2023-07-03 15:34:46 -0300

    seedeo de packages

--- routes/admin.ts
+++ routes/admin.ts
@@ -3,14 +3,11 @@ import {
   createPackage,
   deletePackage,
   getAllActiveDeliveries,
-  getAllDeliveries,
   getAllPackages,
   updatePackage
 } from "../controllers/adminController";
 const router: Router = express.Router();
 import { isAdmin } from "../middleware/validateMiddleware";
-
-router.get("/deliveries", isAdmin, getAllDeliveries);
 
 router.get("/deliveries/active", isAdmin, getAllActiveDeliveries);
 
