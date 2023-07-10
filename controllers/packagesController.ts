d35e53de84153b29f2a8ca875825a06f2aa2e842
MateoCata98 <mate.catalano98@gmail.com>
2023-07-10 16:41:27 -0300

    fix/services

--- controllers/packagesController.ts
+++ controllers/packagesController.ts
@@ -1,26 +1,22 @@
 import { Request, Response } from "express";
-import { User, Package } from "../models";
+import {
+  selectPackagesService,
+  editPackageService,
+  getAllDeliveryPackagesService,
+  getAllPackagesService,
+  createPackageService,
+  updatePackageService,
+  deletePackageService
+} from "../services/packageService";
 
 export async function selectPackages(req: Request, res: Response) {
   try {
     const userId = req.params.id;
     const packageIds: number[] = req.body;
-    const user: User | null = await User.findByPk(userId);
-    if (!user) {
-      return res.status(404).send({ message: "User not found" });
-    }
-    const packages: Package[] = await Package.findAll({
-      where: {
-        id: packageIds
-      }
-    });
-    if (packageIds.length !== packages.length) {
-      return res.status(400).send({ message: "Some packages do not exist" });
-    }
-    await Package.update({ UserId: user.id }, { where: { id: packageIds } });
+    await selectPackagesService(userId, packageIds);
     return res.status(200).send({ message: "All packages selected" });
-  } catch {
-    return res.status(500).send("Internal Server Error");
+  } catch (error) {
+    return res.status(500).send({ message: (error as Error).message });
   }
 }
 
@@ -28,99 +24,63 @@ export async function editPackage(req: Request, res: Response) {
   try {
     const packageId = req.params.idPackage;
     const userId = req.params.idUser;
-    const [_, [editedPackage]] = await Package.update(req.body, {
-      where: { id: packageId, UserId: userId },
-      returning: true,
-      individualHooks: true
-    });
-    console.log(_);
-    if (!editedPackage) {
-      return res.status(404).send("Package not found");
-    }
+    const editedPackage = await editPackageService(packageId, userId, req.body);
     return res
       .status(200)
       .send({ editedPackage, message: "Package edited successfully" });
-  } catch {
-    return res.status(500).send({ message: "Internal Server Error" });
+  } catch (error) {
+    return res.status(500).send({ message: (error as Error).message });
   }
 }
+
 export async function getAllDeliveryPackages(req: Request, res: Response) {
   try {
     const userId = req.params.id;
-    const packages: Package[] = await Package.findAll({
-      where: {
-        UserId: userId
-      }
-    });
-    if (!packages[0]) {
-      return res.status(404).send({ message: "No packages found" });
-    }
+    const packages = await getAllDeliveryPackagesService(userId);
     return res.status(200).send({ packages, message: "All packages" });
-  } catch {
-    return res.status(500).send({ message: "Internal Server Error" });
+  } catch (error) {
+    return res.status(500).send({ message: (error as Error).message });
   }
 }
 
 export async function getAllPackages(req: Request, res: Response) {
   try {
-    const allPackages = await Package.findAll();
-    if (!allPackages) {
-      return res.status(404).send({ message: "Packages not found" });
-    }
+    const allPackages = await getAllPackagesService();
     return res.status(200).send({ allPackages, message: "All packages" });
-  } catch {
-    return res.status(500).send({ message: "Internal Server Error" });
+  } catch (error) {
+    return res.status(500).send({ message: (error as Error).message });
   }
 }
 
-export async function createPackage(
-  req: Request,
-  res: Response
-): Promise<Response> {
+export async function createPackage(req: Request, res: Response) {
   try {
-    const newPackage = await Package.create(req.body);
-    if (!newPackage) {
-      return res.status(400).send("Error creating package");
-    }
+    const newPackage = await createPackageService(req.body);
     return res
       .status(201)
       .send({ newPackage, message: "Package created successfully" });
-  } catch {
-    return res.status(500).send({ message: "Internal Server Error" });
+  } catch (error) {
+    return res.status(500).send({ message: (error as Error).message });
   }
 }
 
 export async function updatePackage(req: Request, res: Response) {
   try {
     const packageId = req.params.id;
-    const [_, [editedPackage]] = await Package.update(req.body, {
-      where: { id: packageId },
-      returning: true,
-      individualHooks: true
-    });
-    console.log(_);
-    if (!editedPackage) {
-      return res.status(404).send("Package not found");
-    }
+    const editedPackage = await updatePackageService(packageId, req.body);
     return res
       .status(200)
       .send({ editedPackage, message: "Package updated successfully" });
-  } catch {
-    return res.status(500).send({ message: "Internal Server Error" });
+  } catch (error) {
+    return res.status(500).send({ message: (error as Error).message });
   }
 }
 
 export async function deletePackage(req: Request, res: Response) {
   try {
     const packageId = req.params.id;
-    const editedPackage = await Package.destroy({
-      where: { id: packageId }
-    });
-    if (!editedPackage) {
-      return res.status(404).send("Package not found");
-    }
+    await deletePackageService(packageId);
     return res.status(200).send({ message: "Package deleted successfully" });
-  } catch {
-    return res.status(500).send({ message: "Internal Server Error" });
+  } catch (error) {
+    return res.status(500).send({ message: (error as Error).message });
   }
 }
---
+++ services/packageService.ts
@@ -0,0 +1,118 @@
+import { Package, User } from "../models";
+
+export async function selectPackagesService(
+  userId: string,
+  packageIds: number[]
+) {
+  try {
+    const user: User | null = await User.findByPk(userId);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    const packages: Package[] = await Package.findAll({
+      where: {
+        id: packageIds
+      }
+    });
+    if (packageIds.length !== packages.length) {
+      throw new Error("Some packages do not exist");
+    }
+    await Package.update({ UserId: user.id }, { where: { id: packageIds } });
+    return "All packages selected";
+  } catch (error) {
+    throw new Error("Internal Server Error");
+  }
+}
+
+export async function editPackageService(
+  packageId: string,
+  userId: string,
+  packageData: any
+) {
+  try {
+    const [_, [editedPackage]] = await Package.update(packageData, {
+      where: { id: packageId, UserId: userId },
+      returning: true,
+      individualHooks: true
+    });
+    if (!editedPackage) {
+      throw new Error("Package not found");
+    }
+    return editedPackage;
+  } catch (error) {
+    throw new Error("Internal Server Error");
+  }
+}
+
+export async function getAllDeliveryPackagesService(userId: string) {
+  try {
+    const packages: Package[] = await Package.findAll({
+      where: {
+        UserId: userId
+      }
+    });
+    if (!packages[0]) {
+      throw new Error("No packages found");
+    }
+    return packages;
+  } catch (error) {
+    throw new Error("Internal Server Error");
+  }
+}
+
+export async function getAllPackagesService() {
+  try {
+    const allPackages = await Package.findAll();
+    if (!allPackages) {
+      throw new Error("Packages not found");
+    }
+    return allPackages;
+  } catch (error) {
+    throw new Error("Internal Server Error");
+  }
+}
+
+export async function createPackageService(packageData: any) {
+  try {
+    const newPackage = await Package.create(packageData);
+    if (!newPackage) {
+      throw new Error("Error creating package");
+    }
+    return newPackage;
+  } catch (error) {
+    throw new Error("Internal Server Error");
+  }
+}
+
+export async function updatePackageService(
+  packageId: string,
+  packageData: any
+) {
+  try {
+    const [_, [editedPackage]] = await Package.update(packageData, {
+      where: { id: packageId },
+      returning: true,
+      individualHooks: true
+    });
+    if (!editedPackage) {
+      throw new Error("Package not found");
+    }
+    return editedPackage;
+  } catch (error) {
+    throw new Error("Internal Server Error");
+  }
+}
+
+export async function deletePackageService(packageId: string) {
+  try {
+    const deletedPackage = await Package.destroy({
+      where: { id: packageId }
+    });
+    if (!deletedPackage) {
+      throw new Error("Package not found");
+    }
+    return "Package deleted";
+  } catch (error) {
+    throw new Error("Internal Server Error");
+  }
+}
